1c98ca2fab955b8a5db1d04e40a066990d0c6dac
codebaard <github@juliusneudecker.com>
2021-07-18 22:52:05 +0200

    refinded target script

--- Scripts/targetHitDurationDistribution.py
+++ Scripts/targetHitDurationDistribution.py
@@ -3,22 +3,30 @@ from LoadResults import open as loadCSV
 import matplotlib.pyplot as plt
 import pandas as pd
 import os, re
+import csv
 
-def main():
-    print("Im alive")
-    # load log file
-    # aggregate logfile data into dataframe
+data = list()
 
-    # for filename in os.listdir(FileNames.resultRoot):
-    #     try:
-    #         regex = re.compile(r'\d+')
-    #         number = regex.findall(filename)
-    #         df = loadCSV(filename)
-    #
-    #
-    #     except Exception as e:
-    #         print(e)
+for filename in os.listdir(FileNames.logfiles):
+    try:
+        regex = re.compile(r'\d+')
+        number = regex.findall(filename)
+        file_CSV = open(FileNames.logfiles + filename)
+        data_CSV = csv.reader(file_CSV, delimiter = ';')
+        list_CSV = list(data_CSV)
 
+        for row in list_CSV:
+            if row[1] == 'Model.NeuroTagMarkedAsTargetLogEntry':
+                row = number + row
+                data.append(row)
+            elif row[1] == 'Model.NeuroTagHitLogEntry':
+                row = number + row
+                data.append(row)
 
-if __name__ == 'main':
-    main()
+    except Exception as e:
+        print(e)
+
+df = pd.DataFrame(data)
+df.columns = ['participant', 'timestamp', 'eventtype', 'value']
+
+print(df)
