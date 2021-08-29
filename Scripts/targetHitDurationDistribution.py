43a9466ff59f0935af0aeff7029ca720a82e672a
codebaard <github@juliusneudecker.com>
2021-08-29 11:25:59 +0200

    hit time evaluation

--- Scripts/targetHitDurationDistribution.py
+++ Scripts/targetHitDurationDistribution.py
@@ -49,3 +49,24 @@ logData = pd.DataFrame(combinedData)
 
 print(combinedData)
 
+hitTimes = list()
+
+for index, row in enumerate(combinedData):
+    result = 0
+    if row[2] == 'Model.NeuroTagMarkedAsTargetLogEntry':
+        if combinedData[index+1][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+1][3] == row[3]:
+            result = int(combinedData[index+1][1]) - int(row[1])
+        if combinedData[index+2][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+2][3] == row[3]:
+            result = int(combinedData[index+2][1]) - int(row[1])
+        if combinedData[index+3][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+3][3] == row[3]:
+            result = int(combinedData[index+3][1]) - int(row[1])
+
+    hitSet = [row[0], row[3], result, row[4], row[5], row[6]]
+
+    hitTimes.append(hitSet)
+
+print(hitTimes)
+
+
+
+
