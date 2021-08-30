d5a1874681eae40dc2f13b9cf495021c96b7c2cc
codebaard <github@juliusneudecker.com>
2021-08-30 23:11:28 +0200

    added skewed target times

--- Scripts/targetHitDurationDistribution.py
+++ Scripts/targetHitDurationDistribution.py
@@ -14,7 +14,7 @@ for filename in os.listdir(FileNames.logfiles):
         regex = re.compile(r'\d+')
         number = regex.findall(filename)
         file_CSV = open(FileNames.logfiles + filename)
-        data_CSV = csv.reader(file_CSV, delimiter = ';')
+        data_CSV = csv.reader(file_CSV, delimiter=';')
         list_CSV = list(data_CSV)
 
         for index, row in enumerate(list_CSV):
@@ -30,42 +30,46 @@ for filename in os.listdir(FileNames.logfiles):
         print(e)
 
 for index, row in enumerate(data):
-    if index < (len(data)-2) and data[index+1][2] != 'Model.NeuroTagHitLogEntry' and data[index+2][2] != 'Model.NeuroTagHitLogEntry':
-        newDataset = copy.deepcopy(data[index+1])
+    if index < (len(data) - 2) and data[index + 1][2] != 'Model.NeuroTagHitLogEntry' and data[index + 2][
+        2] != 'Model.NeuroTagHitLogEntry':
+        newDataset = copy.deepcopy(data[index + 1])
         newDataset[2] = 'Model.NeuroTagHitLogEntry'
-        newDataset[3] = data[index-1][3]
-        data.insert(index+2, newDataset)
-
+        newDataset[3] = data[index - 1][3]
+        data.insert(index + 2, newDataset)
 
 surveyData = pd.read_csv(FileNames.userSurvey, sep=';',
-                           header=0, encoding='ascii', engine='python')
+                         header=0, encoding='ascii', engine='python')
 combinedData = list()
 for row in data:
-    set = surveyData.loc[surveyData['participant'] == int(row[0]) ]
+    set = surveyData.loc[surveyData['participant'] == int(row[0])]
     setList = set.values.tolist()
     row = row + setList[0][1:]
     combinedData.append(row)
 
 logData = pd.DataFrame(combinedData)
 
-#print(combinedData)
+# print(combinedData)
 
 hitTimes = list()
 
 for index, row in enumerate(combinedData):
     result = 0
     if row[2] == 'Model.NeuroTagMarkedAsTargetLogEntry':
-        if combinedData[index+1][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+1][3] == row[3] and combinedData[index+1][0] == row[0]:
-            result = int(combinedData[index+1][1]) - int(row[1])
-        elif combinedData[index+2][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+2][3] == row[3] and combinedData[index+2][0] == row[0]:
-            result = int(combinedData[index+2][1]) - int(row[1])
-        elif combinedData[index+3][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index+3][3] == row[3] and combinedData[index+3][0] == row[0]:
-            result = int(combinedData[index+3][1]) - int(row[1])
+        if combinedData[index + 1][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index + 1][3] == row[3] and \
+                combinedData[index + 1][0] == row[0]:
+            result = int(combinedData[index + 1][1]) - int(row[1])
+        elif combinedData[index + 2][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index + 2][3] == row[3] and \
+                combinedData[index + 2][0] == row[0]:
+            result = int(combinedData[index + 2][1]) - int(row[1])
+        elif combinedData[index + 3][2] == 'Model.NeuroTagHitLogEntry' and combinedData[index + 3][3] == row[3] and \
+                combinedData[index + 3][0] == row[0]:
+            result = int(combinedData[index + 3][1]) - int(row[1])
 
-        #if result < 120000 and result > 2000:
-        #if result > 2000 and result < 10000: # for reduced window
-        if result > 2000 and result < 10000: # for glasses and gender
-            hitSet = [row[0], row[3], result/1000, row[4], row[5], row[6]]
+        # if result < 120000 and result > 2000:
+        # if result > 2000 and result < 10000: # for reduced window
+        # if result > 2000 and result < 10000: # for glasses and gender
+        if result > 2000 and result < 20000: # for targets
+            hitSet = [row[0], row[3], result / 1000, row[4], row[5], row[6]]
             hitTimes.append(hitSet)
 
 hitTimeData = pd.DataFrame(hitTimes)
@@ -92,22 +96,44 @@ hitTimeData.columns = ['participant', 'target', 'time', 'age', 'gender', 'glasse
 # plt.show()
 
 ## Gender
-male = list()
-female = list()
+# male = list()
+# female = list()
+#
+# for index, row in enumerate(hitTimes):
+#     #row[2] = int(row[2])
+#     if row[4] == 'm': # true
+#         male.append(row[2])
+#     else:
+#         female.append(row[2])
+#
+# #glasses = [np.array(withGlasses)[:, 2] ,np.array(noGlasses)[:,2]]
+# glasses = [male ,female]
+# plt.boxplot(glasses)
+#
+# plt.xticks([1, 2],['Male', 'Female'])
+# plt.ylabel('Time (s)')
+#
+# plt.show()
 
-for index, row in enumerate(hitTimes):
-    #row[2] = int(row[2])
-    if row[4] == 'm': # true
-        male.append(row[2])
-    else:
-        female.append(row[2])
+## Time Distribution for specific numbers
 
-#glasses = [np.array(withGlasses)[:, 2] ,np.array(noGlasses)[:,2]]
-glasses = [male ,female]
-plt.boxplot(glasses)
+zero = hitTimeData.loc[hitTimeData['target'] == '0'].time.to_numpy(dtype=float)
+one = hitTimeData.loc[hitTimeData['target'] == '1'].time.to_numpy(dtype=float)
+two = hitTimeData.loc[hitTimeData['target'] == '2'].time.to_numpy(dtype=float)
+three = hitTimeData.loc[hitTimeData['target'] == '3'].time.to_numpy(dtype=float)
+four = hitTimeData.loc[hitTimeData['target'] == '4'].time.to_numpy(dtype=float)
+five = hitTimeData.loc[hitTimeData['target'] == '5'].time.to_numpy(dtype=float)
+six = hitTimeData.loc[hitTimeData['target'] == '6'].time.to_numpy(dtype=float)
+seven = hitTimeData.loc[hitTimeData['target'] == '7'].time.to_numpy(dtype=float)
+eight = hitTimeData.loc[hitTimeData['target'] == '8'].time.to_numpy(dtype=float)
+nine = hitTimeData.loc[hitTimeData['target'] == '9'].time.to_numpy(dtype=float)
 
-plt.xticks([1, 2],['Male', 'Female'])
-plt.ylabel('Time (s)')
+plt.boxplot([zero, one, two, three, four, five, six, seven, eight, nine])
+
+#plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9],['0','1','2','3','4','5','6','7','8','9'])
+plt.xticks(np.arange(10),['0','1','2','3','4','5','6','7','8','9'])
+plt.xlabel('Target Number')
+plt.ylabel('Time')
 
 plt.show()
 
@@ -128,7 +154,3 @@ plt.show()
 #
 # plt.grid()
 # plt.show()
-
-
-
-
